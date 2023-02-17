2d6efdaea6bfa530e60500356680eb0e4be92d2c
Niklas Malkusch <niklasmalkusch@googlemail.com>
2023-02-17 15:23:48 +0100

    dark mode, what more do you need?

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 import React from 'react';
 import './App.css';
-import { Grid, Card } from '@mui/material';
+import { Grid, Card, CssBaseline } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material';
 import NavBar from './components/NavBar';
 import Counter from './components/Counter';
 import QuestContainer from './components/QuestContainer';
 
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+
 function App() {
   return (
     <div className="App" id='app'>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={12}>
-          <NavBar />
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={12}>
+            <NavBar />
+          </Grid>
+          <Grid item xs={12} sm={12}>
+            <QuestContainer/>
+          </Grid>
+          <Grid item xs={12} sm={1}>
+            <Card className='Counter'>
+              <Counter />
+            </Card>
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={12}>
-          <QuestContainer/>
-        </Grid>
-        <Grid item xs={12} sm={1}>
-          <Card className='Counter'>
-            <Counter />
-          </Card>
-        </Grid>
-      </Grid>
+      </ThemeProvider>
     </div>
   );
 }
