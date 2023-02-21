5c18aa33c62703681f6a5d9e28cb82ced631a677
Niklas Malkusch <niklasmalkusch@googlemail.com>
2023-02-21 23:03:38 +0100

    removed useless styling again

--- src/components/QuestDetails.js
+++ src/components/QuestDetails.js
@@ -6,18 +6,16 @@ export function QuestDetails({ quest }) {
         return null;
     }
     return (
-        <div style={{ height: '100%', width: '100%' }}>
-            <div className = "QuestDetails" >
-                <h2>{quest.name}</h2>
-                <h4>Quest Type</h4>
-                <p>{quest.type}</p>
-                <h4>Quest Description</h4>
-                <p>{quest.description}</p>
-                <h4>Quest Location</h4>
-                <p>{quest.location}</p>
-                <h4>Quest Reward</h4>
-                <p>{quest.reward}</p>
-            </div>
+        <div className = "QuestDetails" style={{ height: '100%', width: '100%' }}>
+            <h2>{quest.name}</h2>
+            <h4>Quest Type</h4>
+            <p>{quest.type}</p>
+            <h4>Quest Description</h4>
+            <p>{quest.description}</p>
+            <h4>Quest Location</h4>
+            <p>{quest.location}</p>
+            <h4>Quest Reward</h4>
+            <p>{quest.reward}</p>
         </div>
     );
 }
